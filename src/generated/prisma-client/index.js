e3769bd74a5a28e8c062cd265668e73e367bbe40
Manuel Di Cristo <manuel.dicristo@icloud.com>
2019-04-12 14:14:29 -0700

    [+] heroku and schema update

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -9,40 +9,22 @@ var models = [
     embedded: false
   },
   {
-    name: "Product",
-    embedded: false
-  },
-  {
     name: "User",
     embedded: false
   },
   {
     name: "Video",
+    embedded: false
+  },
+  {
+    name: "Product",
     embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://dash-server-ec77ce6bff.herokuapp.com`,
+  endpoint: `https://dash-prisma-server.herokuapp.com/`,
   secret: `uhsomesecretyouhaveuh`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Post",
-    embedded: false
-  },
-  {
-    name: "Product",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Video",
-    embedded: false
-  }
-];
